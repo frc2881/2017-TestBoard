ec40b718072671e873e92b284ef07a138bbbfca7
Lady Cans <frc2881@gmail.com>
2017-11-17 19:00:11 -0600

    i don't know where my changes went so i'm doing this to make me feel safe even if it might just be a placebo effect
    STUDENTS: Tian

--- src/org/usfirst/frc/team2881/robot/Robot.java
+++ src/org/usfirst/frc/team2881/robot/Robot.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team2881.robot;
-
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Spark;
+import org.usfirst.frc.team2881.subsystems.ExpoMotor;
+
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
@@ -21,23 +25,32 @@ import edu.wpi.first.wpilibj.Timer;
  * instead if you're new.
  */
 public class Robot extends SampleRobot {
-	RobotDrive myRobot = new RobotDrive(0, 1); // class that handles basic drive
-												// operations
-	Joystick stick = new Joystick(0); // set to ID 1 in DriverStation
+    RobotDrive myRobot = new RobotDrive(0, 1); // class that handles basic drive
+    // operations
+    SpeedController Chaparral = new Spark(2);
+    SpeedController Ice = new Spark(3);
+    XboxController QuinnExMachina = new XboxController(0); // set to ID 1 in DriverStation
 
-	public Robot() {
-		myRobot.setExpiration(0.1);
-	}
+    ExpoMotor expoMotor = new ExpoMotor(QuinnExMachina);
 
-	/**
-	 * Runs the motors with tank steering.
-	 */
-	@Override
-	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
-		while (isOperatorControl() && isEnabled()) {
-			myRobot.tankDrive(stick, 1, stick, 5);
-			Timer.delay(0.005); // wait for a motor update time
-		}
-	}
+    public Robot() {
+        myRobot.setExpiration(0.1);
+
+    }
+
+    /**
+     * Runs the motors with tank steering.
+     */
+    @Override
+    public void operatorControl() {
+        myRobot.setSafetyEnabled(true);
+
+        while (isOperatorControl() && isEnabled()) {
+            myRobot.tankDrive(QuinnExMachina.getY(Hand.kLeft), QuinnExMachina.getY(Hand.kRight));
+            Chaparral.set(QuinnExMachina.getTriggerAxis(Hand.kLeft));
+            expoMotor.exponential(2, Ice);
+
+            Timer.delay(0.005); // wait for a motor update time
+        }
+    }
 }
--- src/org/usfirst/frc/team2881/subsystems/ExpoMotor.java
+++ src/org/usfirst/frc/team2881/subsystems/ExpoMotor.java
@@ -1,4 +1,48 @@
-package subsystems;
+package org.usfirst.frc.team2881.subsystems;
 
-public class ExpoMotor {
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Spark;
+
+public class ExpoMotor extends SampleRobot {
+
+    //
+    // Fields on the ExpoMotor object are next
+    //
+    XboxController QuinnExMachina;
+
+    //
+    // Constructor for the ExpoMotor object is next, called when someone does "new ExpoMotor(...)"
+    //
+    public ExpoMotor(XboxController QuinnExMachina) {
+        this.QuinnExMachina = QuinnExMachina;
+    }
+
+    //
+    // Methods on the ExpoMotor object below
+    //
+
+    public void exponential(int port, SpeedController spark) {
+        double electron1 = 0;
+        double electron2 = 1;
+        double crement = .5 / 200.0;  // <- this is a local variable, not a field
+
+        if (QuinnExMachina.getRawButton(port) == true) {
+            spark.set(electron1);
+            electron1 = (electron1 + crement) * (1 + .75 / 200);
+        } else if (QuinnExMachina.getRawButton(port) == false && electron1 >= 1 && electron2 > 0) {
+            spark.set(electron2);
+            electron2 = (electron2 - crement) / (1 + .75 / 200);
+        } else {
+            spark.set(0);
+            electron1 = 0;
+            electron2 = 1;
+        }
+        SmartDashboard.putNumber("electron1", electron1);
+        SmartDashboard.putNumber("electron2", electron2);
+        Timer.delay(0.005);
+    }
 }
